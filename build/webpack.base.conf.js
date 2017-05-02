007eb774dcb63b2a0b19e5151396d54bcdcdf5ba
ciotas <linzikristy@qq.com>
2017-05-02 12:26:55 +0800

    reduce an ajax requst

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -63,5 +63,5 @@ let webpackConfig = {
 
 
 module.exports = vuxLoader.merge(webpackConfig, {
-  plugins: ['vux-ui', 'progress-bar', 'duplicate-style']
+  plugins: ['vux-ui', 'progress-bar', 'duplicate-style','inline-manifest']
 })
